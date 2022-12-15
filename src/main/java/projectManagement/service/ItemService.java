80c1a95ab7dc66b2cc8d4785ff63179d6def95da
Dvir Shaul <dvir.shaul@gmail.com>
2022-12-15 17:19:59 +0200

    work on board controller and service

--- src/main/java/projectManagement/service/ItemService.java
+++ src/main/java/projectManagement/service/ItemService.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import projectManagement.entities.Item;
 import projectManagement.entities.Response;
+import projectManagement.repository.BoardRepo;
 import projectManagement.repository.ItemRepo;
 
 @Service
 public class ItemService {
     @Autowired
     ItemRepo itemRepo;
-
+    @Autowired
+    BoardRepo boardRepo;
     public ItemService() {
     }
 
@@ -20,7 +22,28 @@ public class ItemService {
             Response.createFailureResponse("can't crate a new item");
         }
         return Response.createSuccessfulResponse(savedItem);
+    }
 
+    public Response<Item> addItem(long boardId,Item item) {
+        if(boardRepo.findById(boardId).isPresent()){
+            //TODO: add item to board
+            return Response.createSuccessfulResponse(item);
+        }
+        return Response.createFailureResponse("no board id like that");
+    }
+    public Response<Item> removeItem(long boardId,Item item) {
+        if(boardRepo.findById(boardId).isPresent()){
+            //TODO: removeItem from board
+            return Response.createSuccessfulResponse(item);
+        }
+        return Response.createFailureResponse("no board id like that");
+    }
 
+    public Response<Item> assignItemToUser(long boardId,long userId,Item item) {
+        if(boardRepo.findById(boardId).isPresent()){
+            //TODO: assignItemToUser
+            return Response.createSuccessfulResponse(item);
+        }
+        return Response.createFailureResponse("no board id like that");
     }
 }
