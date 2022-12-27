5c2f185978c55228a762b58ae92ef3cb85dba4f9
khader-zatari <khaderzatari@gmail.com>
2022-12-27 19:18:35 +0200

    tests on item controller class

--- src/test/java/projectManagement/controller/ItemControllerTest.java
+++ src/test/java/projectManagement/controller/ItemControllerTest.java
@@ -242,7 +242,7 @@ public class ItemControllerTest {
         given(boardService.userExistsInBoard(board, assignedToUser)).willReturn(Response.createSuccessfulResponse(UserRole.USER));
         Response<Item> successfulResponse = Response.createSuccessfulResponse(newItem);
         given(itemService.changeAssignedToUser(oldItem.getId(), assignedToUser, board)).willReturn(successfulResponse);
-
+        //todo check if i should add these three rows.
 //        given(itemService.changeStatus(oldItem.getId(), newStatus, board)).willReturn(successfulResponse);
 //        Set<Long> allUserInBoard = Stream.of(1L, 2L, 3L).collect(Collectors.toCollection(HashSet::new));
 //        given(boardService.getAllUsersInBoardByBoardId(board.getId())).willReturn(allUserInBoard);
@@ -316,53 +316,109 @@ public class ItemControllerTest {
     }
 
     @Test
-    public void deleteComment_withValidItemId_returnSuccessResponse() {
-//        String title = "new item";
-//        String status = "new status";
-//        String newComment = "new comment";
-//
-//        Item item = new Item(title, status, board, user);
-//        Comment comment = new Comment(newComment, user, item, LocalDateTime.now());
-//        Item newItem = new Item(title, status, board, user);
-//        Set<Comment> allComments = Stream.of(comment).collect(Collectors.toCollection(HashSet::new));
-//        newItem.setComments(allComments);
-//
-//        Response<Item> successfulResponse = Response.createSuccessfulResponse(newItem);
-//        given(itemService.addComment(item.getId(), board, user, newComment)).willReturn(successfulResponse);
-//
-//
-//        Set<Long> allUserInBoard = Stream.of(1L, 2L, 3L).collect(Collectors.toCollection(HashSet::new));
-//        given(boardService.getAllUsersInBoardByBoardId(board.getId())).willReturn(allUserInBoard);
-//
-//
-//        ResponseEntity<String> resItem = itemController.addComment(user, board, item.getId(), newComment);
-//
-//
-//        verify(socketsUtil).updateItem(successfulResponse.getData(), board.getId());
-//        verify(notificationService).sendNotification(allUserInBoard, "add commentnew item new comment is added new comment", board.getId(), NotifyWhen.ITEM_COMMENT_ADDED);
-//
-//
-//        assertEquals(HttpStatus.OK, resItem.getStatusCode());
-//        assertEquals(newItem, successfulResponse.getData());
+    public void deleteComment_withValidCommentId_returnSuccessResponse() {
+        long commentId = 2;
+        Item item = new Item("new item", "new status", board, user);
+        Response<Item> successfulResponse = Response.createSuccessfulResponse(item);
+        given(itemService.deleteComment(board, user, commentId)).willReturn(successfulResponse);
 
-        assertEquals(true, false);
+
+        ResponseEntity<String> resItem = itemController.deleteComment(user, board, commentId);
+
+
+        verify(socketsUtil).updateItem(successfulResponse.getData(), board.getId());
+
+
+        assertEquals(HttpStatus.OK, resItem.getStatusCode());
+        assertEquals(item, successfulResponse.getData());
+
+
     }
 
     @Test
-    public void deleteComment_withInValidItemId_returnBadRequestResponse() {
-//        long itemId = 2;
-//        String newComment = "new comment";
-//        Response<Item> failureResponse = Response.createFailureResponse(null);
-//        given(itemService.addComment(itemId, board, user, newComment)).willReturn(failureResponse);
-//
-//        ResponseEntity<String> responseAddComment = itemController.addComment(user, board, itemId, newComment);
-//        assertEquals(HttpStatus.BAD_REQUEST, responseAddComment.getStatusCode());
-        assertEquals(false, true);
+    public void deleteComment_withInValidCommentId_returnBadRequestResponse() {
+        long commentId = 2;
+
+        Response<Item> failureResponse = Response.createFailureResponse(null);
+        given(itemService.deleteComment(board, user, commentId)).willReturn(failureResponse);
+
+        ResponseEntity<String> responseDeleteComment = itemController.deleteComment(user, board, commentId);
+        assertEquals(HttpStatus.BAD_REQUEST, responseDeleteComment.getStatusCode());
 
 
     }
 
 
+    @Test
+    public void updateItemImportance_withValidItemId_returnSuccessResponse() {
 
+        Item item = new Item("new item", "new status", board, user);
+        Item newItem = new Item("new item2", "new status", board, user);
+        newItem.setImportance(ItemImportance.ONE)
+        ;
+        Response<Item> successfulResponse = Response.createSuccessfulResponse(newItem);
+        given(itemService.updateImportance(board , user , item.getId() , ItemImportance.ONE)).willReturn(successfulResponse);
+
+
+        ResponseEntity<String> res = itemController.updateItemImportance(user, board, item.getId(), ItemImportance.ONE);
+
+
+        verify(socketsUtil).updateItem(successfulResponse.getData(), board.getId());
+
+
+        assertEquals(HttpStatus.OK, res.getStatusCode());
+        assertEquals(newItem, successfulResponse.getData());
+
+
+    }
+
+    @Test
+    public void updateImportance_withInValidItemId_returnBadRequestResponse() {
+        long commentId = 2;
+
+        Response<Item> failureResponse = Response.createFailureResponse(null);
+        given(itemService.deleteComment(board, user, commentId)).willReturn(failureResponse);
+
+        ResponseEntity<String> responseDeleteComment = itemController.deleteComment(user, board, commentId);
+        assertEquals(HttpStatus.BAD_REQUEST, responseDeleteComment.getStatusCode());
+
+
+    }
+
+    @Test
+    public void createSubItem_withParentId_returnSuccessResponse() {
+        String childTitle = "chile item";
+        Item item = new Item("new item", "new status", board, user);
+        Item newItem = new Item(childTitle, "new status", board, user);
+//        newItem.setImportance(ItemImportance.ONE)
+
+        Response<Item> successfulResponse = Response.createSuccessfulResponse(newItem);
+        given(itemService.createSubItem(childTitle , user , board , item.getId())).willReturn(successfulResponse);
+
+
+        ResponseEntity<String> res = itemController.createSubItem(user , board , item.getId() , childTitle);
+
+
+        verify(socketsUtil).createItem(successfulResponse, board.getId());
+
+
+        assertEquals(HttpStatus.OK, res.getStatusCode());
+        assertEquals(newItem, successfulResponse.getData());
+
+
+    }
+
+    @Test
+    public void createSubItem_withInValidParentId_returnBadRequestResponse() {
+        long parentId = 2;
+
+        Response<Item> failureResponse = Response.createFailureResponse(null);
+        given(itemService.createSubItem("no title",user ,board, parentId)).willReturn(failureResponse);
+
+        ResponseEntity<String> responseDeleteComment = itemController.createSubItem(user, board,parentId , "no title");
+        assertEquals(HttpStatus.BAD_REQUEST, responseDeleteComment.getStatusCode());
+
+
+    }
 
 }
